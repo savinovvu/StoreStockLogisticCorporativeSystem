408ff0e4633b61757468a5709a32b2414bb52ea2
savinovvu <savinov_vu@inbox.ru>
2016-12-04 15:39:34 +0800

    Add EnumStatusProduct "Комментарий" for save comment.

--- src/main/java/ru/inbox/savinov_vu/model/goods/status/EnumStatusProduct.java
+++ src/main/java/ru/inbox/savinov_vu/model/goods/status/EnumStatusProduct.java
@@ -1,6 +1,7 @@
 package ru.inbox.savinov_vu.model.goods.status;
 
 public enum EnumStatusProduct  {
+    Комментарий,
     Обработан, Не_обработан, Отменен,
     Заказан, Не_заказан,
     Выявлен_дефект, Проверен_и_отложен, Отсутствует,
